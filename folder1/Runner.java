e0653a62b562ef62dfd5a2ca3f7240a0a524506d
kayadav <kamalkrishna.er@gmail.com>
2019-05-31 09:33:47 +0530

    added runner file

---
+++ folder1/Runner.java
@@ -0,0 +1,15 @@
+package com.gitCommand;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Application: "+args[0]);
+		System.out.println("Module: "+args[1]);
+		System.out.println("Platform: "+args[2]);
+		System.out.println("Environment: "+args[3]);
+		System.out.println("Browser: "+args[4]);
+		System.out.println("Emails: "+args[5]);
+	}
+
+}
